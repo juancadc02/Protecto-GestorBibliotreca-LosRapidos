a2bea5520199cb92f225325bf0869a62fc1670b6
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-11-29 14:31:45 +0100

    colocacion vistas

--- Poyecto-Gestor-Biblioteca-Web-Los-Rapidos/wwwroot/js/site.js
+++ Poyecto-Gestor-Biblioteca-Web-Los-Rapidos/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function revisarContraseña() {
+    var contraseña = document.getElementById('contrasenya').value;
+    var confContraseña = document.getElementById('confContrasenya').value;
+    var mensajeContraseña = document.getElementById('mensajeContrasenya');
+
+    if (contraseña === confContraseña) {
+        mensajeContraseña.textContent = 'Las contraseñas coinciden';
+        mensajeContraseña.style.color = 'green';
+        document.getElementById("btnRegistro").disabled = false;//habilita el boton
+    } else {
+        mensajeContraseña.textContent = 'Las contraseñas no coinciden';
+        mensajeContraseña.style.color = 'red';
+        document.getElementById("btnRegistro").disabled = true;//deshabilita el boton
+    }
+}
